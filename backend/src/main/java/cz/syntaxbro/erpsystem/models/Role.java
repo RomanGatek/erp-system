66de2682249a73cce46a71e985dbe880103bc7b7
CoffeeRequired <nhl-jurasek@seznam.cz>
2025-01-24 22:21:25 +0100

    initials feat: prepare for review

---
+++ backend/src/main/java/cz/syntaxbro/erpsystem/models/Role.java
@@ -0,0 +1,66 @@
+package cz.syntaxbro.erpsystem.models;
+
+import jakarta.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "role_permissions",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "permission_id")
+    )
+    private Set<Permission> permissions;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    public Role() {
+    }
+
+    public Role(String name, Set<Permission> permissions) {
+        this.name = name;
+        this.permissions = permissions;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Permission> getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(Set<Permission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+}
---
+++ backend/src/main/java/cz/syntaxbro/erpsystem/repositories/RoleRepository.java
@@ -0,0 +1,10 @@
+package cz.syntaxbro.erpsystem.repositories;
+
+import cz.syntaxbro.erpsystem.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
