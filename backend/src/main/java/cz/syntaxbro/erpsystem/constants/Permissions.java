4067a3d8374018c9315375e88224ef7347b6f83c
CoffeeRequired <nhl-jurasek@seznam.cz>
2025-01-24 22:16:34 +0100

    // REPAIR

--- backend/src/main/java/cz/syntaxbro/erpsystem/constants/Permissions.java
+++
@@ -1,7 +0,0 @@
-package cz.syntaxbro.erpsystem.constants;
-
-public class Permissions {
-    public static final String READ_REPORTS = "READ_REPORTS";
-    public static final String APPROVE_BUDGETS = "APPROVE_BUDGETS";
-    public static final String VIEW_PROFILE = "VIEW_PROFILE";
-}
--- backend/src/main/java/cz/syntaxbro/erpsystem/constants/Roles.java
+++
@@ -1,7 +0,0 @@
-package cz.syntaxbro.erpsystem.constants;
-
-public class Roles {
-    public static final String ADMIN = "ROLE_ADMIN";
-    public static final String MANAGER = "ROLE_MANAGER";
-    public static final String USER = "ROLE_USER";
-}
