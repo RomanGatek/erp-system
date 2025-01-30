57dd2516c6830584cc7be530260556641d6d294d
Roman Gatek <roman.gatek@gmail.com>
2025-01-30 20:15:40 +0100

    Update UserDto. Added LoginRequest and SingUpRequest.

--- backend/src/main/java/cz/syntaxbro/erpsystem/models/dtos/UserDto.java
+++ backend/src/main/java/cz/syntaxbro/erpsystem/models/dtos/UserDto.java
@@ -1,7 +1,17 @@
 package cz.syntaxbro.erpsystem.models.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
 import java.util.Set;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
 
     private Long id;
@@ -9,62 +19,7 @@ public class UserDto {
     private String firstName;
     private String lastName;
     private String email;
-    private boolean isActive;
-    private Set<String> roles;
+    private boolean active;
+    private Set<String> roles = new HashSet<>();
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public boolean isActive() {
-        return isActive;
-    }
-
-    public void setActive(boolean active) {
-        isActive = active;
-    }
-
-    public Set<String> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<String> roles) {
-        this.roles = roles;
-    }
 }
