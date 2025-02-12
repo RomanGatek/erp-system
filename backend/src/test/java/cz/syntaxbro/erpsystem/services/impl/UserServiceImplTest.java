6fd8f7fc4b832a82b14ba83645ce625b733d04af
christosste86 <christosste86@gmail.com>
2025-02-12 21:30:33 +0100

    add test findByUsernameFound Not Found

--- backend/src/test/java/cz/syntaxbro/erpsystem/services/impl/UserServiceImplTest.java
+++ backend/src/test/java/cz/syntaxbro/erpsystem/services/impl/UserServiceImplTest.java
@@ -16,15 +16,17 @@ import org.springframework.test.context.ActiveProfiles;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ActiveProfiles("test")
 class UserServiceImplTest {
 
     UserServiceImpl userServiceImpl;
+    User user;
 
     @Mock
     UserRepository userRepository;
@@ -38,6 +40,7 @@ class UserServiceImplTest {
     void setUp() {
         autoCloseable = MockitoAnnotations.openMocks(this);
         userServiceImpl = new UserServiceImpl(userRepository, roleRepository);
+        this.user = new User(1L,"username", "1!Password", "firstName", "lastName", "email@email", true, Set.of());
     }
 
     @AfterEach
@@ -48,9 +51,8 @@ class UserServiceImplTest {
     @Test
     void getAllUsers() {
         //Arrange
-        User user = new User(1L,"username", "1!Password", "firstName", "lastName", "email@email", true, Set.of());
         List<User> mockUsers = new ArrayList<>();
-        mockUsers.add(user);
+        mockUsers.add(this.user);
         when(userRepository.findAll()).thenReturn(mockUsers);
         //Act
         List<UserDto> users = userServiceImpl.getAllUsers();
@@ -60,11 +62,34 @@ class UserServiceImplTest {
     }
 
     @Test
-    void getUserByUsername() {
+    void getUserByUsernameFound() {
+        //Arrange
+        when(userRepository.findByUsername("username")).thenReturn(Optional.of(this.user));
+        //Act
+        User result = userServiceImpl.getUserByUsername("username");
+        //Assert
+        assertNotNull(result);
+        assertEquals("username", result.getUsername());
+        verify(userRepository, times(1)).findByUsername("username");
+    }
+
+    @Test
+    void getUserByUsernameNotFound() {
+        //Arrange
+        when(userRepository.findByUsername("username")).thenReturn(Optional.empty());
+        //Act
+        User result = userServiceImpl.getUserByUsername("username");
+        //Assert
+        assertNull(result);
+        verify(userRepository, times(1)).findByUsername("username");
     }
 
     @Test
     void getUserById() {
+        //Arrest
+        when(userRepository.findById(1L)).thenReturn(Optional.of(this.user));
+        //Act
+
     }
 
     @Test
