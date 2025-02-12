fee60a1f2ecfa420694306d725a1b1a1422f5ac1
christosste86 <christosste86@gmail.com>
2025-02-12 21:14:40 +0100

    add test to getAllUsers

---
+++ backend/src/test/java/cz/syntaxbro/erpsystem/services/impl/UserServiceImplTest.java
@@ -0,0 +1,81 @@
+package cz.syntaxbro.erpsystem.services.impl;
+
+
+import cz.syntaxbro.erpsystem.models.User;
+import cz.syntaxbro.erpsystem.models.dtos.UserDto;
+import cz.syntaxbro.erpsystem.repositories.RoleRepository;
+import cz.syntaxbro.erpsystem.repositories.UserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ActiveProfiles("test")
+class UserServiceImplTest {
+
+    UserServiceImpl userServiceImpl;
+
+    @Mock
+    UserRepository userRepository;
+
+    @Mock
+    RoleRepository roleRepository;
+
+    AutoCloseable autoCloseable;
+
+    @BeforeEach
+    void setUp() {
+        autoCloseable = MockitoAnnotations.openMocks(this);
+        userServiceImpl = new UserServiceImpl(userRepository, roleRepository);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        autoCloseable.close();
+    }
+
+    @Test
+    void getAllUsers() {
+        //Arrange
+        User user = new User(1L,"username", "1!Password", "firstName", "lastName", "email@email", true, Set.of());
+        List<User> mockUsers = new ArrayList<>();
+        mockUsers.add(user);
+        when(userRepository.findAll()).thenReturn(mockUsers);
+        //Act
+        List<UserDto> users = userServiceImpl.getAllUsers();
+        //Assert
+        assertEquals(1, users.size());
+        verify(userRepository, times(1)).findAll();
+    }
+
+    @Test
+    void getUserByUsername() {
+    }
+
+    @Test
+    void getUserById() {
+    }
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+}
