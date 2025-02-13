8aad8ba27aa31ef71e9a4b3ded87261393d0a926
tiy <x.tiy@pm.me>
2025-02-13 18:17:12 +0100

    added dependencies for JWT

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -30,6 +30,11 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
+    // JWT
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+
     // Database Driver
     runtimeOnly("com.mysql:mysql-connector-j")
     runtimeOnly("com.h2database:h2")
