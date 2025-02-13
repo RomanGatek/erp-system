fa05a12bd56ec00107fe9f486ff41645094dd1a5
I have an idea <49106207+christosste86@users.noreply.github.com>
2025-02-13 17:43:14 +0100

    create test for repo findOrderBetweenDates

--- backend/src/test/java/cz/syntaxbro/erpsystem/repositories/OrderRepositoryTest.java
+++ backend/src/test/java/cz/syntaxbro/erpsystem/repositories/OrderRepositoryTest.java
@@ -1,43 +1,73 @@
 package cz.syntaxbro.erpsystem.repositories;
 
 import cz.syntaxbro.erpsystem.models.Order;
-import cz.syntaxbro.erpsystem.models.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class OrderRepositoryTest {
 
-    private final RoleRepository roleRepository;
-
     @Autowired
-    OrderRepositoryTest(RoleRepository roleRepository) {
-        this.roleRepository = roleRepository;
-    }
+    private OrderRepository orderRepository;
+
 
     @BeforeEach
     void setUp() {
-        //Products
-        Product productOne = new Product(1L, "ProductOne", 100.0, 100);
-        Product productTwo = new Product(1L, "ProductTwo", 200.0, 50);
-        //Date of orders
-        LocalDateTime orderDateOne = LocalDateTime.of(2025,2,13, 10, 20);
-        LocalDateTime orderDateTwo = LocalDateTime.of(2025,2,5, 9, 20);
-        //Orders
-        Order orderOne = new Order(1L, productOne, 2, 200, Order.Status.ORDERED, orderDateOne);
-        Order orderTwo = new Order(1L, productTwo, 5, 1000, Order.Status.ORDERED, orderDateTwo);
+
+        // Date of orders
+        LocalDateTime orderDateOne = LocalDateTime.of(2025, 2, 13, 10, 20);
+        LocalDateTime orderDateTwo = LocalDateTime.of(2025, 2, 5, 9, 20);
+
+        // Orders
+        Order orderOne = new Order(null, null, 2, 200, Order.Status.ORDERED, orderDateOne);
+        Order orderTwo = new Order(null, null, 5, 1000, Order.Status.ORDERED, orderDateTwo);
+
+        // Save Orders
+        orderRepository.save(orderOne);
+        orderRepository.save(orderTwo);
     }
 
     @Test
-    void findByCostBetween() {
+    void findByCostBetweenWithOneResult() {
+        // Act
+        List<Order> orders = orderRepository.findByCostBetween(100.0, 300.0);
+
+        // Assert
+        assertEquals(1, orders.size());
+        assertEquals(200, orders.getFirst().getCost());
+    }
+
+    @Test
+    void findByCostBetweenWithTwoResults() {
+        // Act
+        List<Order> orders = orderRepository.findByCostBetween(100.0, 1000.0);
+
+        // Assert
+        assertEquals(2, orders.size());
+    }
+
+    @Test
+    void findByDateBetweenOrderDateWithOneResult() {
         //Act
+        List<Order> orders = orderRepository.findByDateBetween(
+                LocalDateTime.of(2025, 2, 7, 0, 0),
+                LocalDateTime.of(2025, 2, 14, 0, 0));
+        assertEquals(1, orders.size());
+    }
 
-
+    @Test
+    void findByDateBetweenOrderDateWithTwoResult() {
+        //Act
+        List<Order> orders = orderRepository.findByDateBetween(
+                LocalDateTime.of(2025, 2, 1, 0, 0),
+                LocalDateTime.of(2025, 2, 25, 0, 0));
+        assertEquals(2, orders.size());
     }
 }
