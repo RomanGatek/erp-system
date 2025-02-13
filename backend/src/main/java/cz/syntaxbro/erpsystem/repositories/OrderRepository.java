b70d825e7dd0501bc623bd49808ec28d42159c66
I have an idea <49106207+christosste86@users.noreply.github.com>
2025-02-13 16:30:14 +0100

    create Query findByCostBetween()

--- backend/src/main/java/cz/syntaxbro/erpsystem/repositories/OrderRepository.java
+++ backend/src/main/java/cz/syntaxbro/erpsystem/repositories/OrderRepository.java
@@ -2,9 +2,14 @@ package cz.syntaxbro.erpsystem.repositories;
 
 import cz.syntaxbro.erpsystem.models.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-
+    @Query("select o from Order o where o.cost between :cost and :cost2")
+    List<Order> findByCostBetween(@Param("cost") Double costOne, @Param("cost2") Double costTwo);
 }
