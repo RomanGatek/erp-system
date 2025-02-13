f3bd4f33eb7d0a863aa1cc9175c11ccab418ef05
I have an idea <49106207+christosste86@users.noreply.github.com>
2025-02-13 19:14:25 +0100

    add update delete Order Service

--- backend/src/main/java/cz/syntaxbro/erpsystem/services/impl/OrderServiceImpl.java
+++ backend/src/main/java/cz/syntaxbro/erpsystem/services/impl/OrderServiceImpl.java
@@ -2,51 +2,66 @@ package cz.syntaxbro.erpsystem.services.impl;
 
 import cz.syntaxbro.erpsystem.models.Order;
 import cz.syntaxbro.erpsystem.models.Product;
+import cz.syntaxbro.erpsystem.repositories.OrderRepository;
 import cz.syntaxbro.erpsystem.services.OrderService;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrderServiceImpl implements OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public OrderServiceImpl(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
     @Override
-    public Order getOrderById(int id) {
-        return null;
+    public Order getOrderById(Long id) {
+        Optional<Order> orderOptional =  orderRepository.findById(id);
+        if (orderOptional.isPresent()) {
+            return orderOptional.get();
+        }return null;
     }
 
     @Override
     public List<Order> getOrders() {
-        return List.of();
+        return orderRepository.findAll();
     }
 
     @Override
     public List<Order> getOrdersByCostBetween(double start, double end) {
-        return List.of();
+        return orderRepository.findByCostBetween(start, end);
     }
 
     @Override
-    public List<Order> getOrdersByDateBetween(Date start, Date end) {
-        return List.of();
+    public List<Order> getOrdersByDateBetween(LocalDateTime start, LocalDateTime end) {
+        return orderRepository.findByDateBetween(start,end);
     }
 
     @Override
     public List<Order> getOrdersByProduct(Product product) {
-        return List.of();
+        return orderRepository.findByProduct(product);
     }
 
     @Override
     public void createOrder(Order order) {
-
+        orderRepository.save(order);
     }
 
     @Override
-    public void updateOrder(Order order) {
-
+    public void updateOrder(Long id, Order order) {
+        Optional<Order> OrderOptional = orderRepository.findById(id);
+        if (OrderOptional.isPresent()) {
+            orderRepository.save(order);
+        }
     }
 
     @Override
-    public void deleteOrder(long id) {
-
+    public void deleteOrder(Long id) {
+        orderRepository.deleteById(id);
     }
 }
