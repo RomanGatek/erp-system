77a9e968205408669e978332a9126d3cf85438dc
christosste86 <christosste86@gmail.com>
2025-02-13 15:15:05 +0100

    create OrderController

---
+++ backend/src/main/java/cz/syntaxbro/erpsystem/controllers/OrderController.java
@@ -0,0 +1,10 @@
+package cz.syntaxbro.erpsystem.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/order")
+public class OrderController {
+
+}
