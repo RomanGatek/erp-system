35e93d0af8c55bf268e28c725b9e310e2616941a
0tiy <x.tiy@pm.me>
2025-02-15 14:48:46 +0100

    created JwtUtil, token generation method, added app.properties field for secret key

---
+++ backend/src/main/java/cz/syntaxbro/erpsystem/utils/JwtUtil.java
@@ -0,0 +1,41 @@
+package cz.syntaxbro.erpsystem.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    // Injected from application.properties
+    @Value("${jwt_secret}")
+    private String secret;
+
+    private Key getSigningKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(String username) {
+        long expirationMillis = 3600000;     // 1 hour
+        Date now = new Date();
+        Date expiration = new Date(now.getTime() + expirationMillis);
+
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(now)
+                .expiration(expiration)
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+
+
+
+}
