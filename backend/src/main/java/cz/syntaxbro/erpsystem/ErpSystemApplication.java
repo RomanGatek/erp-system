473a1028c42a158d1603fa3f4402807de5ebc39c
CoffeeRequired <nhl-jurasek@seznam.cz>
2025-02-15 10:13:28 +0100

    remove basePath

--- backend/src/main/java/cz/syntaxbro/erpsystem/ErpSystemApplication.java
+++ backend/src/main/java/cz/syntaxbro/erpsystem/ErpSystemApplication.java
@@ -2,10 +2,7 @@ package cz.syntaxbro.erpsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
 @SpringBootApplication
-@ComponentScan(basePackages = "cz.syntaxbro.erpsystem")
 public class ErpSystemApplication {
 
     public static void main(String[] args) {
