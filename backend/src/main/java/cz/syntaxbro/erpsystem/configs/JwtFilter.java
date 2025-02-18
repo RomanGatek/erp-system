7a12f83b4a732b5cc8d36f12e294f2d5527d1394
0tiy <x.tiy@pm.me>
2025-02-18 19:48:34 +0100

    JwtFilter initial setup

---
+++ backend/src/main/java/cz/syntaxbro/erpsystem/configs/JwtFilter.java
@@ -0,0 +1,24 @@
+package cz.syntaxbro.erpsystem.configs;
+
+import cz.syntaxbro.erpsystem.utils.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    public JwtFilter(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
