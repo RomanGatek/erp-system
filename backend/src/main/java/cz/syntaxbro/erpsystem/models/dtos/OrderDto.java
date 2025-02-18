cf0390f9aece4f20c9058fd6b01854bdc88056f4
christosste86 <christosste86@gmail.com>
2025-02-18 12:01:11 +0100

    add validate OrderDto values NotBlank and not null

--- backend/src/main/java/cz/syntaxbro/erpsystem/models/dtos/OrderDto.java
+++ backend/src/main/java/cz/syntaxbro/erpsystem/models/dtos/OrderDto.java
@@ -1,9 +1,11 @@
 package cz.syntaxbro.erpsystem.models.dtos;
 
 import cz.syntaxbro.erpsystem.models.Order;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import java.time.LocalDateTime;
 
@@ -11,9 +13,16 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderDto {
+    @NotBlank(message = "Amount is required")
     private int amount;
+    @NotBlank(message = "Cost is required")
     private double cost;
+    @NotBlank(message = "Status is required")
+    @NonNull
     private Order.Status status;
+    @NonNull
     private LocalDateTime orderTime;
+    @NotBlank(message = "Product is required")
+    @NonNull
     private Long productId;
 }
