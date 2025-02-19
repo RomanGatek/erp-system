0d0088b082018804fff0efbf9264d2aa0400580a
0tiy <x.tiy@pm.me>
2025-02-19 19:17:41 +0100

    implement MyUserDetailsService

---
+++ backend/src/main/java/cz/syntaxbro/erpsystem/services/impl/MyUserDetailsService.java
@@ -0,0 +1,23 @@
+package cz.syntaxbro.erpsystem.services.impl;
+
+import cz.syntaxbro.erpsystem.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public MyUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
