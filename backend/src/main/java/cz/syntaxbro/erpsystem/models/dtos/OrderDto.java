a069dee051553d773052645ca78c43fd1569608e
christosste86 <christosste86@gmail.com>
2025-02-21 11:03:15 +0100

    create validation for order data

--- backend/src/main/java/cz/syntaxbro/erpsystem/models/dtos/OrderDto.java
+++ backend/src/main/java/cz/syntaxbro/erpsystem/models/dtos/OrderDto.java
@@ -1,11 +1,13 @@
 package cz.syntaxbro.erpsystem.models.dtos;
 
 import cz.syntaxbro.erpsystem.models.Order;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 
 import java.time.LocalDateTime;
 
@@ -16,11 +18,18 @@ public class OrderDto {
 
     private Integer amount;
 
+    @NotBlank
+    @Size(min = 1, message = "Cost cannot be null, must be greater than 0")
     private Double cost;
 
+    @NotBlank
     private Order.Status status;
 
+    @NotBlank
+    @FutureOrPresent
     private LocalDateTime orderTime;
 
+    @NotBlank
+    @Min(1)
     private Long productId;
 }
