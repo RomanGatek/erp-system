7487fab47d86da8f3efbfb8f5b43bf2fee9d40b2
coffee <nhl-jurasek@seznam.cz>
2025-02-27 22:16:54 +0100

    add fe tests via VITESTS

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vite.dev/config/1
 export default defineConfig({
   plugins: [
     vue(),
