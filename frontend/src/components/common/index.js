0073ac5622e67df9b2887dbda215daf6c60e029a
coffee <nhl-jurasek@seznam.cz>
2025-03-03 13:36:02 +0100

    create & utilized barell files

---
+++ frontend/src/components/common/index.js
@@ -0,0 +1,11 @@
+// index.js (barrel file)
+export { default as BaseCheckbox } from './BaseCheckbox.vue'
+export { default as BaseInput } from './BaseInput.vue'
+export { default as DataTable } from './DataTable.vue'
+export { default as EmptyState } from './EmptyState.vue'
+export { default as Modal } from './Modal.vue'
+export { default as NotificationWrapper } from './NotificationWrapper.vue'
+export { default as Pagination } from './Pagination.vue'
+export { default as PasswordInput } from './PasswordInput.vue'
+export { default as SearchBar } from './SearchBar.vue'
+export { default as StatusBar } from './StatusBar.vue'
