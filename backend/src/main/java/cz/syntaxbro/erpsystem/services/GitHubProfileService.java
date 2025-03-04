46b3d6d7639f4470d2cc6de37b667ecafd2444bf
I have an idea <49106207+christosste86@users.noreply.github.com>
2025-03-04 23:22:09 +0100

    public only one method createGitHubProfile()

--- backend/src/main/java/cz/syntaxbro/erpsystem/services/GitHubProfileService.java
+++ backend/src/main/java/cz/syntaxbro/erpsystem/services/GitHubProfileService.java
@@ -1,12 +1,5 @@
 package cz.syntaxbro.erpsystem.services;
 
-import cz.syntaxbro.erpsystem.models.gitHub.Branch;
-import cz.syntaxbro.erpsystem.models.gitHub.GitHubProfile;
-
-import java.util.List;
-
 public interface GitHubProfileService {
-    public List<String> projectPartners();
-    public List<Branch> projectBranches();
-    public GitHubProfile gitHubProfile(String username);
+    public void createGitHubProfile();
 }
