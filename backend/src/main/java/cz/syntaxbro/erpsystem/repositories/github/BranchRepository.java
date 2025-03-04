a9fc291febb797e0aea2295fe247d143fb1f8ed2
I have an idea <49106207+christosste86@users.noreply.github.com>
2025-03-04 23:19:55 +0100

    add findByName to repo

--- backend/src/main/java/cz/syntaxbro/erpsystem/repositories/github/BranchRepository.java
+++ backend/src/main/java/cz/syntaxbro/erpsystem/repositories/github/BranchRepository.java
@@ -4,6 +4,10 @@ import cz.syntaxbro.erpsystem.models.gitHub.Branch;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BranchRepository extends JpaRepository<Branch, Long> {
+
+    Optional<Branch> findByName(String name);
 }
