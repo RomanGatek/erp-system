13064978d52843b954919499e0ee2d870b2c81b2
coffee <nhl-jurasek@seznam.cz>
2025-03-05 21:10:01 +0100

    add initialised tests for fer

--- frontend/vitest.config.js
+++ frontend/vitest.config.js
@@ -1,7 +1,6 @@
 /// <reference types="vitest" />
 
-import { fileURLToPath } from 'node:url'
-import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
+import { mergeConfig, defineConfig } from 'vitest/config'
 import viteConfig from './vite.config'
 import vue from '@vitejs/plugin-vue'
 
@@ -11,9 +10,7 @@ export default mergeConfig(
     plugins: [vue()],
     test: {
       globals: true,
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url)),
+      environment: 'jsdom'
     },
   }),
 )
