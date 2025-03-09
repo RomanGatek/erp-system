1539f9ab2cd5ccbf8a7bc667516820fa97459720
I have an idea <49106207+christosste86@users.noreply.github.com>
2025-03-09 22:17:57 +0100

    add findByName query

--- backend/src/main/java/cz/syntaxbro/erpsystem/repositories/ProductCategoryRepository.java
+++ backend/src/main/java/cz/syntaxbro/erpsystem/repositories/ProductCategoryRepository.java
@@ -2,8 +2,15 @@ package cz.syntaxbro.erpsystem.repositories;
 
 import cz.syntaxbro.erpsystem.models.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+
+    @Query("select pc from ProductCategory pc where pc.name = :categoryName")
+    Optional<ProductCategory> findByName(@Param("categoryName") String categoryName);
 }
