730d5eb14659937efe4e867c1898676fbae5e24e
I have an idea <49106207+christosste86@users.noreply.github.com>
2025-03-09 22:48:36 +0100

    create controller with endPoints get, getAll, delete, create, delete, update

---
+++ backend/src/main/java/cz/syntaxbro/erpsystem/controllers/ProductCategoryController.java
@@ -0,0 +1,63 @@
+package cz.syntaxbro.erpsystem.controllers;
+
+import cz.syntaxbro.erpsystem.models.ProductCategory;
+import cz.syntaxbro.erpsystem.services.ProductCategoryService;
+import cz.syntaxbro.erpsystem.services.ProductService;
+import jakarta.validation.constraints.NotEmpty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/category")
+@EnableMethodSecurity()
+@PreAuthorize("hasAnyRole('MANAGER', 'ADMIN')")
+public class ProductCategoryController {
+
+    private final ProductCategoryService productCategoryService;
+
+    @Autowired
+    public ProductCategoryController(ProductCategoryService productCategoryService) {
+        this.productCategoryService = productCategoryService;
+    }
+
+    @GetMapping("/id")
+    public ResponseEntity<ProductCategory> getProductCategoryById(@RequestParam("id") long id) {
+        return ResponseEntity.ok(
+                productCategoryService.getProductCategory(id)
+        );
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductCategory>> findAll() {
+        return ResponseEntity.ok(
+                productCategoryService.getProductCategories()
+        );
+    }
+
+    @PostMapping()
+    public ResponseEntity<ProductCategory> create(@RequestBody ProductCategory productCategory) {
+        ProductCategory savedProductCategory =  productCategoryService.createProductCategory(productCategory);
+        return ResponseEntity.ok(savedProductCategory);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductCategory> update(
+            @PathVariable long id,
+            @RequestBody ProductCategory productCategory) {
+
+        return ResponseEntity.ok(
+                productCategoryService.updateProductCategory(id, productCategory)
+        );
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> delete(@PathVariable long id) {
+        productCategoryService.deleteProductCategory(id);
+        return ResponseEntity.noContent().build();
+    }
+}
