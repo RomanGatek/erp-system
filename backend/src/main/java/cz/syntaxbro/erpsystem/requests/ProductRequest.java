2956a6d041785492802c6029cfa82748d07253cc
coffee <nhl-jurasek@seznam.cz>
2025-03-12 21:53:42 +0100

    be - product

--- backend/src/main/java/cz/syntaxbro/erpsystem/requests/ProductRequest.java
+++ backend/src/main/java/cz/syntaxbro/erpsystem/requests/ProductRequest.java
@@ -3,10 +3,14 @@ package cz.syntaxbro.erpsystem.requests;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class ProductRequest {
 
