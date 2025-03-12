d3c9ad67c5bf24a0efce77ed4533c6bb99c3e617
I have an idea <49106207+christosste86@users.noreply.github.com>
2025-03-12 15:51:09 +0100

    update inventoryServiceTest to 99%, problem with releaseStock

--- backend/src/test/java/cz/syntaxbro/erpsystem/services/InventoryServiceTest.java
+++ backend/src/test/java/cz/syntaxbro/erpsystem/services/InventoryServiceTest.java
@@ -2,29 +2,32 @@ package cz.syntaxbro.erpsystem.services;
 
 import cz.syntaxbro.erpsystem.models.InventoryItem;
 import cz.syntaxbro.erpsystem.models.Product;
+import cz.syntaxbro.erpsystem.models.ProductCategory;
 import cz.syntaxbro.erpsystem.repositories.InventoryRepository;
 import cz.syntaxbro.erpsystem.services.impl.InventoryServiceImpl;
 import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-@ExtendWith(MockitoExtension.class)
+@Slf4j
 public class InventoryServiceTest {
 
     @Mock
     private InventoryRepository inventoryRepository;
 
+    @Spy
     @InjectMocks
     private InventoryServiceImpl inventoryService;
 
@@ -34,6 +37,7 @@ public class InventoryServiceTest {
 
     @BeforeEach
     void setUp() {
+        MockitoAnnotations.openMocks(this);
         this.product = Product.builder()
                 .id(1L)
                 .name("Test Product")
@@ -87,4 +91,233 @@ public class InventoryServiceTest {
 
         verify(inventoryRepository, times(1)).updateQuantity(itemId, newQuantity);
     }
+
+    @Test
+    void testGetAll_Successfully(){
+        //Arrest
+        long id = 1L;
+        when(inventoryRepository.findById(id)).thenReturn(Optional.of(item1));
+        when(inventoryRepository.findAll()).thenReturn(List.of(this.item1));
+        //Act
+        List<InventoryItem> result = inventoryService.getAll();
+        //Assert
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        verify(inventoryService, times(1)).getAll();
+    }
+
+    @Test
+    void findItemByProductForOrder_Successfully(){
+        //Arrest
+        when(inventoryRepository.findByProduct(this.product)).thenReturn(Optional.of(this.item1));
+        //Act
+        Optional<InventoryItem> result = inventoryService.findItemByProductForOrder(this.product);
+        //Assert
+        assertNotNull(result);
+        assertEquals(product.getName(), result.get().getProduct().getName());
+        verify(inventoryService, times(1)).findItemByProductForOrder(this.product);
+    }
+
+    @Test
+    void testGetItem_successfully(){
+        //Arrest
+        long id = 1L;
+        when(inventoryRepository.findById(id)).thenReturn(Optional.of(item1));
+        //Act
+        InventoryItem result = inventoryService.getItem(id);
+        //Assert
+        assertNotNull(result);
+        assertEquals(id, result.getId());
+        verify(inventoryService, times(1)).getItem(id);
+    }
+
+    @Test
+    void testGetItem_NoFoundInventoryItem(){
+        //Arrest
+        long id = 1L;
+        when(inventoryRepository.findById(id)).thenReturn(Optional.empty());
+        //Act
+        EntityNotFoundException exceptionResult = assertThrows(EntityNotFoundException.class, () ->
+                inventoryService.getItem(id));
+        //Assert
+
+        assertEquals(String.format("Item with id %d not found", id), exceptionResult.getMessage());
+        verify(inventoryService, times(1)).getItem(id);
+    }
+
+    @Test
+    void testFindItemByProduct_Successfully(){
+        //Arrest
+        when(inventoryRepository.findByProduct(this.product)).thenReturn(Optional.of(this.item1));
+        //Act
+        InventoryItem result = inventoryService.findItemByProduct(this.product);
+        //Assert
+        assertNotNull(result);
+        assertEquals(this.product.getName(), result.getProduct().getName());
+        verify(inventoryService, times(1)).findItemByProduct(this.product);
+    }
+
+    @Test
+    void testFindItemByProduct_NotFoundProduct(){
+        //Arrest
+
+        when(inventoryRepository.findByProduct(this.product)).thenReturn(Optional.empty());
+        //Act
+        EntityNotFoundException exceptionResult = assertThrows(EntityNotFoundException.class, () ->
+                inventoryService.findItemByProduct(this.product));
+        //Assert
+        assertEquals(String.format("Inventory item for product '%s' not found", product.getName()), exceptionResult.getMessage());
+        verify(inventoryService, times(1)).findItemByProduct(this.product);
+    }
+
+    @Test
+    void testIsStockAvailable_forTrue(){
+        //Arrest
+        long id = 1L;
+        when(inventoryRepository.findById(id)).thenReturn(Optional.of(item1));
+        when(inventoryService.getItem(id)).thenReturn(item1);
+        //Act
+        boolean result = inventoryService.isStockAvailable(id, this.item1.getStockedAmount() - 1);
+        //Assert
+        assertTrue(result);
+        verify(inventoryService, times(1)).isStockAvailable(id, this.item1.getStockedAmount() - 1);
+    }
+
+    @Test
+    void testIsStockAvailable_forFalse(){
+        //Arrest
+        long id = 1L;
+        when(inventoryRepository.findById(id)).thenReturn(Optional.of(item1));
+        when(inventoryService.getItem(id)).thenReturn(item1);
+        //Act
+        boolean result = inventoryService.isStockAvailable(id, this.item1.getStockedAmount() + 1);
+        //Assert
+        assertFalse(result);
+        verify(inventoryService, times(1)).isStockAvailable(id, this.item1.getStockedAmount() + 1);
+    }
+
+    @Test
+    void testReceiveStock(){
+        //Arrest
+        long id = 1L;
+        when(inventoryRepository.findById(id)).thenReturn(Optional.of(item1));
+        when(inventoryRepository.save(item1)).thenReturn(item1);
+        //Act
+        inventoryService.receiveStock(id, 10);
+        //Assert
+        assertEquals(20, item1.getStockedAmount());
+        verify(inventoryService, times(1)).receiveStock(id, 10);
+    }
+
+    @Test
+    void testReleaseStock_successfully(){
+        //Arrest
+        long id = 1L;
+        when(inventoryRepository.findById(id)).thenReturn(Optional.of(item1));
+        when(inventoryRepository.save(item1)).thenReturn(item1);
+        //Act
+        inventoryService.releaseStock(id, 10);
+        //Assert
+        assertEquals(0, item1.getStockedAmount());
+        verify(inventoryService, times(1)).releaseStock(id, 10);
+    }
+
+    @Test
+    void testReleaseStock_WithQuantityGreaterThanAmount(){
+        //Arrest
+        int quantityGreaterThanAmount = this.item1.getStockedAmount() + 1;
+        long id = 1L;
+        when(inventoryRepository.findById(id)).thenReturn(Optional.of(this.item1));
+        //Act
+        ResponseStatusException exceptionResult = assertThrows(ResponseStatusException.class, () ->
+            inventoryService.releaseStock(id, quantityGreaterThanAmount));
+        //Assert
+        assertEquals("not enough quantity of product", exceptionResult.getReason());
+        assertEquals("406 NOT_ACCEPTABLE \"not enough quantity of product\"", exceptionResult.getMessage());
+        verify(inventoryService, times(1)).releaseStock(id, quantityGreaterThanAmount);
+    }
+
+
+
+    @Test
+    void testUpdateItem_Successfully(){
+        long id = 1L;
+        Product productForUpdate = Product.builder()
+                .buyoutPrice(10)
+                .purchasePrice(8)
+                .name("update title")
+                .description("update description")
+                .productCategory(new ProductCategory())
+                .build();
+
+        InventoryItem inventoryItemDto = InventoryItem.builder()
+                .product(productForUpdate)
+                .stockedAmount(20)
+                .build();
+
+        when(inventoryRepository.findById(id)).thenReturn(Optional.of(item1));
+        when(inventoryRepository.save(item1)).thenReturn(item1);
+        //Act
+        InventoryItem result = inventoryService.updateItem(id, inventoryItemDto);
+        //Assert
+        assertNotNull(result);
+        assertEquals(20, result.getStockedAmount());
+        assertEquals(productForUpdate, result.getProduct());
+        verify(inventoryService, times(1)).updateItem(id, inventoryItemDto);
+    }
+
+    @Test
+    void testUpdateItem_NotFoundInventoryItem(){
+        long id = 1L;
+        Product productForUpdate = Product.builder()
+                .buyoutPrice(10)
+                .purchasePrice(8)
+                .name("update title")
+                .description("update description")
+                .productCategory(new ProductCategory())
+                .build();
+
+        InventoryItem inventoryItemDto = InventoryItem.builder()
+                .product(productForUpdate)
+                .stockedAmount(20)
+                .build();
+        //Arrest
+        when(inventoryRepository.findById(id)).thenReturn(Optional.empty());
+        //Act
+        EntityNotFoundException exceptionResult = assertThrows(EntityNotFoundException.class, () ->
+                inventoryService.updateItem(id, inventoryItemDto));
+        assertEquals("Inventory Item not found with id: " + id, exceptionResult.getMessage());
+        verify(inventoryService, times(1)).updateItem(id, inventoryItemDto);
+    }
+
+    @Test
+    void TestReserveStock_WhenQuantitySmallerThan0(){
+        //Arrest
+        long id = 1L;
+        when(inventoryRepository.findById(id)).thenReturn(Optional.of(item1));
+        //Act
+        int greaterQuantityThanAmount = item1.getStockedAmount()+1;
+        IllegalArgumentException exceptionResult = assertThrows(IllegalArgumentException.class, () ->
+                inventoryService.reserveStock(id, greaterQuantityThanAmount));
+        //Assert
+        assertEquals("We do not have enough quantity of this item.", exceptionResult.getMessage());
+        verify(inventoryService, times(1)).reserveStock(id, greaterQuantityThanAmount);
+    }
+
+    @Test
+    void TestReserveStock_WhenQuantityGreaterOrEqual0WithRowEquals0(){
+        //Arrest
+        long id = 1L;
+        int lesserQuantityThanAmount = item1.getStockedAmount()-1;
+        when(inventoryRepository.findById(id)).thenReturn(Optional.of(item1));
+        when(inventoryRepository.updateQuantity(id, lesserQuantityThanAmount)).thenReturn(0);
+        //Act
+        EntityNotFoundException exeptionResult = assertThrows(EntityNotFoundException.class, () ->
+            inventoryService.reserveStock(id, lesserQuantityThanAmount));
+        //Assert
+        assertEquals(String.format("Item with id %d not found", id), exeptionResult.getMessage());
+        verify(inventoryService, times(1)).reserveStock(id, lesserQuantityThanAmount);
+    }
+
+
 }
