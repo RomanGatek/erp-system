c90aa92888638cb70444236018b17b70a77ae5da
I have an idea <49106207+christosste86@users.noreply.github.com>
2025-03-14 18:57:52 +0100

    add to repo findByCompanyName Query

--- backend/src/main/java/cz/syntaxbro/erpsystem/repositories/SupplierRepository.java
+++ backend/src/main/java/cz/syntaxbro/erpsystem/repositories/SupplierRepository.java
@@ -2,9 +2,16 @@ package cz.syntaxbro.erpsystem.repositories;
 
 import cz.syntaxbro.erpsystem.models.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 public interface SupplierRepository extends JpaRepository<Supplier, Long> {
+
+    @Query("select s from Supplier s where s.companyName = :companyName")
+    Optional<Supplier> findByCompanyName(@Param("companyName") String companyName);
 }
