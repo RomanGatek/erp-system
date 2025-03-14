92b2b843639ace4a2365e9143bd6b9e5d2293fde
I have an idea <49106207+christosste86@users.noreply.github.com>
2025-03-14 18:34:14 +0100

    add annotation Builder, Data

--- backend/src/main/java/cz/syntaxbro/erpsystem/models/Supplier.java
+++ backend/src/main/java/cz/syntaxbro/erpsystem/models/Supplier.java
@@ -2,11 +2,19 @@ package cz.syntaxbro.erpsystem.models;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Entity
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Supplier {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
